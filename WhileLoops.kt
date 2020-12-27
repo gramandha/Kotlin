f119e6ca90eccf196238475d88feb2d8bc540fa8
anagdesa <wega.guitar@gmail.com>
2020-12-27 12:47:58 +0700

    While Loops

---
+++ WhileLoops.kt
@@ -0,0 +1,16 @@
+/*
+while merupakan perulangan yang sangat flesible, 
+dimana dilakukan cek kondisi,
+ketika kondisi tru maka blok while dan teru diulang hingga bernilai false
+
+ */
+
+ fun main() {
+     var i = 0
+
+     while( i < 10){
+         println("Tampilkan perulangan ke $i")
+         i++
+     }
+    println("Perulangan Selesai")
+ }
