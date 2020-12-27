ec34a879ef6ab7e9aefb0622fa09f401c191175f
anagdesa <wega.guitar@gmail.com>
2020-12-27 15:18:47 +0700

    Single Expression Function

---
+++ SingleExpressionFucntion.kt
@@ -0,0 +1,12 @@
+/*
+Single Expression function yaitu deklarasi function hanya dengan 1 baris kode
+Untuk membuat single expression function, kita hanya butuh tanda = (sama dengan)
+setelah deklarasi nama function dan tipe data pengembalian function
+ */
+
+
+ fun kaliDua(a: Int): Int = a*2
+
+ fun main(){
+     println(kaliDua(2))
+ }
