967fb327356fb114c0b60135106cfad51157645c
anagdesa <wega.guitar@gmail.com>
2020-12-27 14:50:24 +0700

    Unit Returning Function

---
+++ UnitReturningFunction.kt
@@ -0,0 +1,19 @@
+/*
+Function ada 2 jenis, yaitu 
+function yang tidak mengembalikan nilai
+kedua function yang mengembalikan nilai
+
+function yang kita buat sebelumnya merupakan fucntion yang mengembalikan data Unit
+dimana Unit merupakan tanda bahwa fucntion tersebut tidak mengembalikan apa-apa
+
+Penulisan Unit tidak wajib, tetpai jika kita menulis tipe data pengembalian sebuah function
+maka secara otomatis dia adalah Unit
+ */
+
+fun printFungsiku(): Unit{
+    println("Hello")
+}
+
+ fun main() {
+     printFungsiku()
+ }
