484bcd5f4e6d788ce23a201be41183b364958de9
anagdesa <wega.guitar@gmail.com>
2020-12-27 12:52:50 +0700

    Do While Loops

---
+++ DoWhileLoops.kt
@@ -0,0 +1,13 @@
+/*
+Pda Do While Loop, kode blok akan dijalankan dahulu, 
+baru diakhir dilakukan pengecekan kondisi
+ */
+
+ fun main() {
+     var i = 4
+     do{
+         println("Perulangan ke-: $i")
+         i++
+
+     }while(i<5)
+ }
