d4bc70498736637cd047cde94757eb86c2d6800c
Gramandha Wega Intyanto <47823515+anagdesa@users.noreply.github.com>
2020-12-27 18:03:31 +0700

    Single Expression Function

--- SingleExpressionFunction.kt
+++ SingleExpressionFunction.kt
@@ -4,7 +4,6 @@ Untuk membuat single expression function, kita hanya butuh tanda = (sama dengan)
 setelah deklarasi nama function dan tipe data pengembalian function
  */
 
-
  fun kaliDua(a: Int): Int = a*2
 
  fun panggil(nama: String): Unit = println("Saya $nama")
