b34964250f8059da00cc74f2071ff9903b4dde27
anagdesa <wega.guitar@gmail.com>
2020-12-27 13:46:56 +0700

    Function

---
+++ Function.kt
@@ -0,0 +1,19 @@
+/*
+Salah satu contoh function yaitu function main
+yaitu-> fun main(){
+
+}
+
+function sebuah blok yang digunakan berulang-ulang pada program
+cara yaitu dengan kunci fun diikuti nama function
+setelah membuat kita bisa mengeksekusi dengan kunci nama functionnya
+ */
+
+ fun helloWorld(){
+     println("Hello World")
+     println("Test Program")
+ }
+
+ fun main(){
+     helloWorld()
+ }
